35b3599066cbc99bfa26ad13eac165f78460c013
Mateusz Soroka <xmsoroka@gmail.com>
2018-10-06 10:00:42 +0200

    Testing git from home

--- zad01/src/pl/mateuszsoroka/javaee/zad01/HelloWorld.java
+++ zad01/src/pl/mateuszsoroka/javaee/zad01/HelloWorld.java
@@ -3,7 +3,7 @@ package pl.mateuszsoroka.javaee.zad01;
 public class HelloWorld {
     public static void main(String arg[]) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
 
     }
 }
